63f4c8cc3fe85d53b9a9070252d567b262c2e5ae
kayxean <36327242+kayxean@users.noreply.github.com>
2025-12-22 22:01:13 +0700

    chore: update extensions structure

--- tools/copy-page-url/background.js
+++ tools/copy-page-url/background.js
@@ -1,36 +1,23 @@
-/**
- * Function to be executed in the context of the active tab.
- * It copies the provided URL to the user's clipboard.
- * NOTE: This function is injected into the active tab's page,
- * so it has full access to the page's Document Object Model (DOM).
- * @param {string | undefined} url The URL string to copy.
- */
-function copyUrlToClipboard(url) {
+async function copyUrlToClipboard(url) {
   if (!url) {
-    console.error("No URL provided to copy.");
+    console.error('No URL provided to copy.');
     return;
   }
 
-  const tempElement = document.createElement("textarea");
-  tempElement.value = url;
-  tempElement.style.position = "fixed";
-  tempElement.style.top = "0";
-  tempElement.style.left = "0";
-  tempElement.style.opacity = "0";
-  document.body.appendChild(tempElement);
-
-  tempElement.select();
-  document.execCommand("copy");
-  document.body.removeChild(tempElement);
-  // console.log(`Quick URL Copy: URL copied successfully: ${url}`);
+  try {
+    await navigator.clipboard.writeText(url);
+    console.log('URL copied to clipboard successfully!');
+  } catch (err) {
+    console.error('Failed to copy occurred in the injected tab: ', err);
+  }
 }
 
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "copy-url") {
+  if (command === 'copy-url') {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const currentTab = tabs[0];
 
-      if (currentTab && currentTab.id) {
+      if (currentTab?.id) {
         chrome.scripting.executeScript({
           target: { tabId: currentTab.id },
           func: copyUrlToClipboard,
