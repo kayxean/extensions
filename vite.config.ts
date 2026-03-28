7431ecea3f746a2c736f3440f3de307c07edf0a5
kayxean <36327242+kayxean@users.noreply.github.com>
2026-03-28 17:53:43 +0700

    chore: migrate to vite plus

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite-plus';
+
+export default defineConfig({
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  fmt: {
+    singleQuote: true,
+    semi: true,
+    experimentalSortPackageJson: true,
+  },
+});
